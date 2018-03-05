10422f6ed738dbe74ea6e9da4cb9319b5fc9b516
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2018-03-05 16:40:00 +0100

    UI: fix typo

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Subscription.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_Subscription.php
@@ -9,7 +9,7 @@ $L['subscriptions_plans'] = 'Pricing and plans';
 $L['subscriptions_info'] = 'The NethServer Subscriptions by Nethesis enables exclusive access to the Stable Updates repository, monitoring tools and immediate professional support services for your NethServer deployments.
 It enables you to successfully deploy, monitor and manage your NethServer instances, complimenting your IT resources and increasing the efficiency and reliability of your service.';
 $L['Register_label'] = 'Register now';
-$L['paste_label'] = 'Do you already have a registration key? Copy and past it in the field below.';
+$L['paste_label'] = 'Do you already have a registration key? Copy and paste it in the field below.';
 $L['SystemId_label'] = 'System ID';
 $L['register_now_label'] = 'Register now';
 $L['Created_label'] = 'Created';
