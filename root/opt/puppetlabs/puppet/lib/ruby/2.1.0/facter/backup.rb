626e2dce4db90df3a90d0649c2c96e8a62c49705
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2018-10-04 09:07:18 +0200

    inventory: retrieve backup-data status (#16)
    Find the most recent log file and parse it.
    
    NethServer/dev#5598

--- root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/backup.rb
+++ root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/backup.rb
@@ -6,16 +6,16 @@ Facter.add('backup') do
         backup = {}
         backup_status = Facter::Core::Execution.exec('/sbin/e-smith/config getprop backup-data status')
         if backup_status == 'enabled'
-            log_file = Facter::Core::Execution.exec('/sbin/e-smith/config getprop backup-data LogFile')
-            if FileTest.exists?(log_file)
-                backup['last'] = {}
-                backup['last']['type'] = Facter::Core::Execution.exec('/sbin/e-smith/config getprop backup-data VFSType')
-                File.open(log_file, 'rb') do |f|
-                    while line = f.gets
-                        backup['last']['start'] = Regexp.last_match(1) if line =~ /^StartTime\s(\d+)/
-                        backup['last']['end'] = Regexp.last_match(1) if line =~ /^EndTime\s(\d+)/
-                        backup['last']['ret_code'] = Regexp.last_match(1) if line =~ /^Errors\s(\d+)/
-                    end
+            # Find most recent log file
+            log_file = Dir.glob("/var/log/backup/backup-backup-data-*").max_by {|f| File.mtime(f)}
+            backup['last'] = {}
+            backup['last']['type'] = Facter::Core::Execution.exec('/sbin/e-smith/config getprop backup-data VFSType')
+            # If no log has been found, facter will catch the exception
+            File.open(log_file, 'rb') do |f|
+                while line = f.gets
+                    backup['last']['start'] = Regexp.last_match(1) if line =~ /^StartTime\s(\d+)/
+                    backup['last']['end'] = Regexp.last_match(1) if line =~ /^EndTime\s(\d+)/
+                    backup['last']['ret_code'] = Regexp.last_match(1) if line =~ /^Errors\s(\d+)/
                 end
             end
         end
