7493c0d251ec6986c22dfd3525fa0836804bb59d
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2019-10-21 17:56:04 +0200

    inventory: retrieve info on latest backup (#27)
    NethServer/dev#5880

--- root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/backup.rb
+++ root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/backup.rb
@@ -1,5 +1,6 @@
 # backup.rb
 
+require 'date'
 Facter.add('backup') do
     confine osfamily: 'RedHat'
     setcode do
@@ -7,15 +8,27 @@ Facter.add('backup') do
         backup_status = Facter::Core::Execution.exec('/sbin/e-smith/db backups getprop backup-data status')
         if backup_status == 'enabled'
             # Find most recent log file
-            log_file = Dir.glob("/var/log/backup/backup-backup-data-*").max_by {|f| File.mtime(f)}
+            log_file = Dir.glob("/var/log/backup/backup-*").max_by {|f| File.mtime(f)}
             backup['last'] = {}
             backup['last']['type'] = Facter::Core::Execution.exec('/sbin/e-smith/db backups getprop backup-data VFSType')
             # If no log has been found, facter will catch the exception
             File.open(log_file, 'rb') do |f|
                 while line = f.gets
-                    backup['last']['start'] = Regexp.last_match(1) if line =~ /^StartTime\s(\d+)/
-                    backup['last']['end'] = Regexp.last_match(1) if line =~ /^EndTime\s(\d+)/
-                    backup['last']['ret_code'] = Regexp.last_match(1) if line =~ /^Errors\s(\d+)/
+                    if line =~ /^Backup started at (.*)$/
+                        date = DateTime.parse(Regexp.last_match(1))
+                        backup['last']['start'] = ''+date.to_time.to_i.to_s
+                    end
+                    if line =~ /^Backup ended at (.*)$/
+                        date = DateTime.parse(Regexp.last_match(1))
+                        backup['last']['end'] = date.to_time.to_i.to_s
+                    end
+                    if line =~ /^Backup status: (.*)$/
+                        if Regexp.last_match(1) == 'SUCCESS'
+                            backup['last']['ret_code'] = 0.to_s
+                        else
+                            backup['last']['ret_code'] = 1.to_s
+                        end
+                    end
                 end
             end
         end
