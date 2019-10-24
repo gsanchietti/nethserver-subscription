e78d3e132254bfd1b6cdf0c3705bdc3460130f3b
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2019-10-24 10:06:12 +0200

    inventory. improved facter backup script (#28)
    Nethserver/dev#5880

--- root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/backup.rb
+++ root/opt/puppetlabs/puppet/lib/ruby/2.1.0/facter/backup.rb
@@ -5,28 +5,43 @@ Facter.add('backup') do
     confine osfamily: 'RedHat'
     setcode do
         backup = {}
-        backup_status = Facter::Core::Execution.exec('/sbin/e-smith/db backups getprop backup-data status')
+        # Find most recent log file
+        log_file = Dir.glob("/var/log/backup/backup-*").max_by {|f| File.mtime(f)}
+
+        # Extract name
+        parts = log_file.split('-')
+        parts.delete('/var/log/backup/backup')
+        parts.pop()
+        name = parts.join('-')
+
+        # Get backup status
+        backup_status = Facter::Core::Execution.exec('/sbin/e-smith/db backups getprop '+name+' status')
         if backup_status == 'enabled'
-            # Find most recent log file
-            log_file = Dir.glob("/var/log/backup/backup-*").max_by {|f| File.mtime(f)}
+            # Get props
             backup['last'] = {}
-            backup['last']['type'] = Facter::Core::Execution.exec('/sbin/e-smith/db backups getprop backup-data VFSType')
+            type = Facter::Core::Execution.exec('/sbin/e-smith/db backups getprop '+name+' VFSType')
+            engine = Facter::Core::Execution.exec('/sbin/e-smith/db backups gettype '+name)
+            backup['last']['type'] = name +' ('+type+'|'+engine+')'
+
             # If no log has been found, facter will catch the exception
             File.open(log_file, 'rb') do |f|
+                dateStart = ""
                 while line = f.gets
                     if line =~ /^Backup started at (.*)$/
-                        date = DateTime.parse(Regexp.last_match(1))
-                        backup['last']['start'] = ''+date.to_time.to_i.to_s
+                        dateStart = DateTime.parse(Regexp.last_match(1))
+                        backup['last']['start'] = ''+dateStart.to_time.to_i.to_s
                     end
                     if line =~ /^Backup ended at (.*)$/
-                        date = DateTime.parse(Regexp.last_match(1))
-                        backup['last']['end'] = date.to_time.to_i.to_s
+                        dateEnd = DateTime.parse(Regexp.last_match(1))
+                        backup['last']['end'] = dateEnd.to_time.to_i.to_s
                     end
                     if line =~ /^Backup status: (.*)$/
                         if Regexp.last_match(1) == 'SUCCESS'
                             backup['last']['ret_code'] = 0.to_s
                         else
+                            # If failed return always 'end' prop, the same of start date
                             backup['last']['ret_code'] = 1.to_s
+                            backup['last']['end'] = dateStart.to_time.to_i.to_s
                         end
                     end
                 end
